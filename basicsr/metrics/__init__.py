c6a3a9c8dcc8e7129fab95fad3af6a487be4910c
CidanShi <157372256+CidanShi@users.noreply.github.com>
2024-02-22 19:58:50 +0800

    Add files via upload

---
+++ basicsr/metrics/__init__.py
@@ -0,0 +1,5 @@
+from .niqe import calculate_niqe
+from .psnr_ssim import calculate_psnr, calculate_ssim
+# from .utils_image import quantize, calc_psnr
+
+__all__ = ['calculate_psnr', 'calculate_ssim', 'calculate_niqe']
